e93d707d6bb7a344f5053245d26c05b2806517b7
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-05-04 17:14:16 +0800

    fix

--- pages/users.php
+++ pages/users.php
@@ -1,5 +1,5 @@
 <?php 
-require_once('../components/navbarDashboard.php')
+require_once '../components/navbarDashboard.php';
 ?>
 
 <body class ="h-screen w-screen">
@@ -92,32 +92,10 @@ require_once('../components/navbarDashboard.php')
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="border-b dark:border-gray-700">
-                            <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple iMac 27&#34;</th>
-                            <td class="px-4 py-3">PC</td>
-                            <td class="px-4 py-3">Apple</td>
-                            <td class="px-4 py-3">300</td>
-                            <td class="px-4 py-3">$2999</td>
-                            <td class="px-4 py-3 flex items-center justify-end">
-                                <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
-                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                    </svg>
-                                </button>
-                                <div id="apple-imac-27-dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                                    <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="apple-imac-27-dropdown-button">
-                                        <li>
-                                            <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Show</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-                                        </li>
-                                    </ul>
-                                    <div class="py-1">
-                                        <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
-                                    </div>
-                                </div>
-                            </td>
+                        <tr class="border-b dark:border-gray-700" id ="usersData">
+                          
+                            
+                         
                         </tr>
                     </tbody>
                 </table>
@@ -170,8 +148,16 @@ require_once('../components/navbarDashboard.php')
 <script defer>
 window.onload = async function(){
 
-  const getUsers =  await fetch("../api/login/login.php");
-  
+  const getUsers =  await fetch("../api/admin/all-users.php");
+  const response = await getUsers.json();
+
+  if(response.responseStatus === 'OK'){
+    let content = '';
+
+    response.responseContent.map((users)=>{
+        content += `<td class="px-4 py-3">`+users.firstname+`</td>`
+    })
+  }
 }
 </script>
 </body>
