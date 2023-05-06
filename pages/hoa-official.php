cc2eb2ab2b5a5fc14fe26afcb1b7da1d7a02f3ba
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-05-06 23:55:22 +0800

    adjust ubutton

--- pages/hoa-official.php
+++ pages/hoa-official.php
@@ -280,14 +280,14 @@ const loadTable = async function(){
         <td class="px-4 py-3">`+users.address+`</td>
         <td class="px-4 py-3">`+user+`</td>
         <td class="px-4 py-3">`+users.created_at+`</td>
-                             <td class="px-4 py-3 flex items-center justify-end">
-                                <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
+        <td class="px-4 py-3 flex items-center justify-end">
+                                <button id="a`+user.id+`-dropdown-button"  class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button" onClick="document.getElementById('a' + '`+user.id+`' + '-dropdown').classList.toggle('hidden')">
                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                     </svg>
                                 </button>
-                                <div id="apple-imac-27-dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                                    <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="apple-imac-27-dropdown-button">
+                                <div id="a`+user.id+`-dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                                    <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="a`+user.id+`-dropdown-button">
                                         <li>
                                             <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Show</a>
                                         </li>
@@ -299,7 +299,7 @@ const loadTable = async function(){
                                         <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
                                     </div>
                                 </div>
-                            </td>
+        </td>
         </tr>
         `
     })
