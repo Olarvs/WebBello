e4a2b9821fb6904ddd75e4f6f618f3bc6113dd53
Olarvs <mauuolarvs@gmail.com>
2023-05-10 20:30:09 +0800

    Fix navigation on admin

--- components/navbarDashboard.php
+++ components/navbarDashboard.php
@@ -62,7 +62,7 @@ if (!isset($_SESSION['ID'])) {
             
             <!-- RESIDENTS -->
             <li>
-                <a href="/web-bello/pages/residents-information.php" id="linkResident" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <a href="/web-bello/pages/resident-information.php" id="linkResident" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <svg aria-hidden="true" class="w-6 h-6 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
                     <span class="ml-3">Resident</span>
                 </a>
