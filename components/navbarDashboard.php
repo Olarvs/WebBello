5a19e2c37c5d6d3af08c7d1db180d184d9e1648d
Olarvs <mauuolarvs@gmail.com>
2023-05-12 00:46:20 +0800

    Removing flowbite logo

--- components/navbarDashboard.php
+++ components/navbarDashboard.php
@@ -79,11 +79,11 @@ $pageName = strtoupper($pageName);
             <span class="sr-only">Toggle sidebar</span>
           </button>
           <a href="https://flowbite.com" class="flex items-center justify-between mr-4">
-            <img
+            <!-- <img
               src="https://flowbite.s3.amazonaws.com/logo.svg"
               class="mr-3 h-8"
               alt="Flowbite Logo"
-            />
+            /> -->
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Web bello</span>
           </a>
      
