b87fb5a1265ef02666162bdebbbb1105100478a2
joegrayson <kingjoelunas@gmail.com>
2023-05-12 02:26:06 +0800

    navbar user minor changes

--- pages/user-forum.php
+++ pages/user-forum.php
@@ -1,6 +1,15 @@
-<?php 
-require_once('../components/navbar.php')
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <link rel="stylesheet" href="../style.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+
+</head>
 
 <body>
 
