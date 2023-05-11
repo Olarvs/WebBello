c2e37b6306342a796861a338cc0f5946116521e4
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-05-11 18:28:31 +0800

    change

--- components/navbarDashboard.php
+++ components/navbarDashboard.php
@@ -13,6 +13,16 @@ if (!isset($_SESSION['ID'])) {
 //store in result
 
 $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+// Get the current URL
+$url = $_SERVER['REQUEST_URI'];
+
+// Extract the page name from the URL
+$pageName = basename($url, '.php');
+
+// Remove hyphens and replace them with spaces
+$pageName = str_replace('-', ' ', $pageName);
+$pageName = strtoupper($pageName);
 }
 ?>
 <!DOCTYPE html>
@@ -30,7 +40,7 @@ $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
 
 <body> 
 <div class="antialiased bg-gray-50 dark:bg-gray-900">
-    <nav class="bg-white border-b border-gray-300 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
+    <nav class="bg-white border-b border-gray-300 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50 ">
       <div class="flex flex-wrap justify-between items-center">
         <div class="flex justify-start items-center">
           <button
@@ -75,35 +85,11 @@ $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
             />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Web bello</span>
           </a>
-          <form action="#" method="GET" class="hidden md:block md:pl-2">
-            <label for="topbar-search" class="sr-only">Search</label>
-            <div class="relative md:w-64 md:w-96">
-              <div
-                class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
-              >
-                <svg
-                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  ></path>
-                </svg>
-              </div>
-              <input
-                type="text"
-                name="email"
-                id="topbar-search"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Search"
-              />
-            </div>
-          </form>
+     
+          
+      
         </div>
+        <h3 class="text-3xl font-bold dark:text-white mx-auto"><?php echo $pageName; ?></h3>
         <div class="flex items-center lg:order-2">
           <button
             type="button"
