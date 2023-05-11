5db954da76317568e40acecc70ec41a60dbdd7e9
joegrayson <kingjoelunas@gmail.com>
2023-05-12 01:51:26 +0800

    User Forum default sidebar id

--- pages/user-forum.php
+++ pages/user-forum.php
@@ -18,7 +18,7 @@ require_once('../components/navbar.php')
         </svg>
     </button>
 
-    <aside id="drawer-navigation"
+    <aside id="default-sidebar"
         class="fixed top-0 left-0 z-40 w-64 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
         aria-label="Sidenav">
         <div
