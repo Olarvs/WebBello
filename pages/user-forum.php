917e5196727ed94d516299ea19c401d3789dff44
Olarvs <mauuolarvs@gmail.com>
2023-05-18 08:52:00 +0800

    Fixing user-index.php

--- pages/user-forum.php
+++ pages/user-forum.php
@@ -20,7 +20,7 @@
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="#" class="flex items-center">
 
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Web-Bello</span>
+                    <!-- <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Web-Bello</span> -->
                 </a>
                 <div class="flex items-center lg:order-2">
                     <a href="#"
@@ -49,7 +49,7 @@
                 </div>
                 <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                     <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                        <li>
+                        <!-- <li>
                             <a href="#"
                                 class="block py-2 pr-4 pl-3 text-dark rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
                                 aria-current="page">Home</a>
@@ -57,7 +57,7 @@
                         <li>
                             <a href="#"
                                 class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Forum</a>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
             </div>
